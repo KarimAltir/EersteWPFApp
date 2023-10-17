6def45a3f0118d9acd6533451857367a4459730e
Karim Altir <mhd.altir@student.ehb.be>
2023-10-17 10:12:25 +0200

    added eventListener for btn2

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace EersteWPFApp
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
